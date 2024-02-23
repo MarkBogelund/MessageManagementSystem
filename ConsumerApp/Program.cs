779b9951f07481593ec10d67dd561fd80c771e9b
Mark Bøgelund <mark.bogelund@BoostPLM.com>
2024-02-23 18:53:51 +0100

    Commented out message sending from program cs

--- ConsumerApp/Program.cs
+++ ConsumerApp/Program.cs
@@ -8,26 +8,28 @@ class Program
 {
     static void Main()
     {
-        //string rabbitMQUri = "amqp://guest:guest@localhost:5672";
-        //string exchangeName = "Message_Broker";
-        //string routingKey = "Broker_key";
-        //string queueName = "Message_queue";
+        string rabbitMQUri = "amqp://guest:guest@localhost:5672";
+        string exchangeName = "Message_Broker";
+        string routingKey = "Broker_key";
+        string queueName = "Message_queue";
 
-        //var rabbitMQConsumer = new RabbitMQConsumer(rabbitMQUri, exchangeName, routingKey, queueName);
-        //rabbitMQConsumer.StartConsuming();
+        var rabbitMQConsumer = new RabbitMQConsumer(rabbitMQUri, exchangeName, routingKey, queueName);
+        rabbitMQConsumer.StartConsuming();
 
-        //Console.WriteLine("Consumer is ready to recieve messages...\n");
-        //Console.WriteLine("Press Enter to stop consuming...");
-        //Console.ReadLine();
+        Console.WriteLine("Consumer is ready to recieve messages...\n");
+        Console.WriteLine("Press Enter to stop consuming...");
+        Console.ReadLine();
 
-        //rabbitMQConsumer.StopConsuming();
+        rabbitMQConsumer.StopConsuming();
 
-        var message = new Message
-        {
-            Timestamp = DateTime.UtcNow,
-            counter = 0
-        };
 
-        Database.InsertMessage(message);
+        // Insert a message into the database
+        //var message = new Message
+        //{
+        //    Timestamp = DateTime.UtcNow,
+        //    counter = 0
+        //};
+
+        //Database.InsertMessage(message);
     }
 }
