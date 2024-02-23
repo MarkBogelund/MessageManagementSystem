b60525c486141bedc9065f715fbb346787856f51
lukas tast <lukasrtast@yahoo.dk>
2024-02-23 16:08:08 +0100

    Init of message broker logic for consumer and producer

--- ConsumerApp/Program.cs
+++ ConsumerApp/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+using System.Threading.Channels;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+ConnectionFactory factory = new();
+
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+factory.ClientProvidedName = "Consumer";
+
+IConnection cnn = factory.CreateConnection();
+
+IModel channel = cnn.CreateModel();
+
+string exchangeName = "Message_Broker";
+string routingKey = "Broker_key";
+string queueName = "Message_queue";
+
+
+channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+channel.QueueDeclare(queueName, true, false, false, null);
+channel.QueueBind(queueName, exchangeName, routingKey, null);
+channel.BasicQos(0, 1, false);
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (sender, args) =>
+{
+    var body = args.Body.ToArray();
+
+    string message = Encoding.UTF8.GetString(body);
+
+    Console.Write($"Message Recieved: {message}");
+
+    channel.BasicAck(args.DeliveryTag, false);
+};
+
+string consumerTag = channel.BasicConsume(queueName, false, consumer);
+
+Console.ReadLine();
+
+channel.BasicCancel(consumerTag);
+
+channel.Close();
+cnn.Close();
--- ProducerApp/Program.cs
+++ ProducerApp/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+ConnectionFactory factory = new();
+
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+factory.ClientProvidedName = "Publisher";
+
+IConnection cnn = factory.CreateConnection();
+
+IModel channel = cnn.CreateModel();
+
+string exchangeName = "Message_Broker";
+string routingKey = "Broker_key";
+string queueName = "Message_queue";
+
+
+channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+channel.QueueDeclare(queueName, true, false, false, null);
+channel.QueueBind(queueName, exchangeName, routingKey, null);
+
+var properties = channel.CreateBasicProperties();
+properties.Persistent = true;
+
+var counter = 0;
+
+for (int i = 0; i < 10; i++)
+{
+
+    byte[] messageBodyBytes = Encoding.UTF8.GetBytes($"Hello message broker, Timestamp of message creation: {DateTime.Now} Counter: {counter} \n");
+    channel.BasicPublish(exchangeName, routingKey, properties, messageBodyBytes);
+    Console.WriteLine($" Sent '{messageBodyBytes}");
+    Thread.Sleep(1000);
+}
+
+channel.Close();
+cnn.Close();
+
