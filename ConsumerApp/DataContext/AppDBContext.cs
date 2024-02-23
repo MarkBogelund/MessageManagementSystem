a867384b506e497fad132651f8847258bb6c4037
Mark Bøgelund <mark.bogelund@BoostPLM.com>
2024-02-23 17:46:37 +0100

    Added connection to database

---
+++ ConsumerApp/DataContext/AppDBContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ConsumerApp.Models;
+
+namespace ConsumerApp.DataContext
+{
+    internal class AppDBContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Host=localhost;Database=messagesDB;Username=postgres;Password=***********;Persist Security Info=True");
+        }
+
+        public DbSet<Message> Messages { get; set; }
+    }
+}
