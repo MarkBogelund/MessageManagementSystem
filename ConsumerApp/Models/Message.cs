f9b072b9b26934d9d169aa37e803e01773181325
Mark Bøgelund <mark.bogelund@BoostPLM.com>
2024-02-23 16:21:46 +0100

    Added packages for entity framework and added Message model

---
+++ ConsumerApp/Models/Message.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsumerApp.Models
+{
+    class Message
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTime Timestamp { get; set; }
+        public int counter;
+    }
+}
