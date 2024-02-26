6e3cd461f7ec797ba658387195dac94350983fe2
lukas tast <lukasrtast@yahoo.dk>
2024-02-26 17:35:41 +0100

    Added unit tests to MessageHandler

--- ConsumerApp.test/MessageHandler.Test.cs
+++ ConsumerApp.test/MessageHandler.Test.cs
@@ -38,19 +38,6 @@ namespace ConsumerApp.test
 
             Assert.That(expectedTimeDifference, Is.EqualTo(result));
         }
-
-        [Test]
-        public void MessageHandler_handleData_CallInsertMessage()
-        {
-            var testMessage = new Message { Id = 1, Counter = 2, Time = 1708887144 }; // seconds are even
-
-            // Act
-            messageHandler.HandleData(testMessage);
-
-            // Assert
-            databaseMock.Verify(x => x.InsertMessage(testMessage), Times.AtLeastOnce);
-        }
-
         [Test]
         public void MessageHandler_HandleMessage_DiscardMessage()
         {
@@ -71,7 +58,63 @@ namespace ConsumerApp.test
         }
 
         [Test]
-        public void MessageHandler_HandleMessage_CallHandleData()
+        public void MessageHandler_HandleMessage_CallInsertMessageThroughHandleData()
+        {
+            // Arrange
+            var testTime = (int)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+
+            // make sure seconds are even
+            if (testTime % 2 == 0)
+            { }
+            else
+            {
+                testTime--;
+            }
+            var testMessage = new Message { Id = 1, Counter = 2, Time = testTime }; 
+
+            //Act
+            messageHandler.HandleMessage(testMessage);
+            
+            // Assert
+            databaseMock.Verify(x => x.InsertMessage(testMessage), Times.Once);
+        }
+
+        [Test]
+        public void MessageHandler_HandleMessage_CallSendMessageBackToQueueThroughHandleData()
+        {
+            // Arrange
+            var testTime = (int)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+
+            // make sure seconds are even
+            if (testTime % 2 == 0)
+            {
+                testTime--;
+
+            }
+            else
+            { }
+            var testMessage = new Message { Id = 1, Counter = 2, Time = testTime };
+
+            //Act
+            messageHandler.HandleMessage(testMessage);
+
+            // Assert
+            messageBrokerMock.Verify(x => x.SendMessageToQueue(testMessage), Times.Once);
+        }
+
+        [Test]
+        public void MessageHandler_handleData_CallInsertMessage()
+        {
+            var testMessage = new Message { Id = 1, Counter = 2, Time = 1708887144 }; // seconds are even
+
+            // Act
+            messageHandler.HandleData(testMessage);
+
+            // Assert
+            databaseMock.Verify(x => x.InsertMessage(testMessage), Times.Once);
+        }
+        [Test]
+        public void MessageHandler_handleData_CallSendBackToQueue()
         {
             var testMessage = new Message { Id = 1, Counter = 2, Time = 1708887145 }; // seconds are odd
 
@@ -79,7 +122,8 @@ namespace ConsumerApp.test
             messageHandler.HandleData(testMessage);
 
             // Assert
-            messageBrokerMock.Verify(x => x.SendMessageToQueue(testMessage), Times.AtLeastOnce);
+            messageBrokerMock.Verify(x => x.SendMessageToQueue(testMessage), Times.Once);
         }
+
     }
 }
